0b1b4d81efac910e6ea01da25217cecd6e0a5b2e
Tomasz Goluch <tomasz.goluch@gmail.com>
2018-06-17 16:49:17 +0200

    Added environment information

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		Console.WriteLine("Hello World !!!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !!!");
+            Console.WriteLine("**Environment**");
+            Console.WriteLine($"Platform: .NET Core");
+            Console.WriteLine($"OS: {RuntimeInformation.OSDescription}");
+        }
+    }
 }
